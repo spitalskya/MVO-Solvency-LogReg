da32b587943af85249ff88085183e501138e7d25
antal36 <antal.tom9@gmail.com>
2024-04-17 21:10:08 +0200

    first bisection implement

--- source/minimization_methods/minimization_in_direction.py
+++ source/minimization_methods/minimization_in_direction.py
@@ -26,8 +26,11 @@ def backtracking(obj_fun: Callable[[np.ndarray], float],
         delta : float
             Parameter of reduction
 
-        x_0 : float
-            starting point
+        x_0 : np.ndarray
+            Starting point
+        
+        s: np.ndarray
+            Direction vector
         
         args : tuple, optional
             Extra arguments passed to the objective function
@@ -79,6 +82,116 @@ def backtracking(obj_fun: Callable[[np.ndarray], float],
     return OptimizeResult(x=lam, nit=it)
     
 def bisection(obj_fun: Callable[[np.ndarray], float],
-              grad: Callable[[np.ndarray], np.ndarray],
-              x_0: np.ndarray, s: np.ndarray) -> OptimizeResult:
-    pass
+              x_0: np.ndarray[float], s: np.ndarray[float], args: tuple=(),
+              grad: Callable[[np.ndarray], np.ndarray]=None,
+              callback: Callable=None, options:dict={}, **kwargs) -> OptimizeResult:
+    """
+
+    Args:
+        obj_fun: Callable[[np.ndarray], float]
+            Objective function
+
+        grad: Callable[[np.ndarray], np.ndarray] 
+            Derivation of objective function
+                If not provided, uses approx_fprime
+
+        x_0: np.ndarray
+            Starting point
+
+        s: np.ndarray
+            Direction
+        args: tuple, optional
+            Extra arguments passed to the objective function.
+
+        callback: Callable, optional
+            Function called after each iteration.
+
+        maxiter: int, optional
+            Maximal number of iterations to perform
+        
+        options : dict, optional
+            A diactionary with solver options.
+                maxiter : int
+                    Maximum number of iterations to perform.
+                tol : float
+                    Tolerance for termination
+        
+        **kwargs: dict, optional
+            Other parameters passed to bisection
+    
+    Raises
+    ------
+    ValueError
+        if 'x_0' is not provided
+
+    Returns
+    -------
+    res : OptimizeResult
+        The optimization result represented as a OptimizeResult object.
+        Important attributes are: x the solution,
+        See OptimizeResult for a description of other attributes.
+    """
+    if x_0 is None:
+        raise ValueError("Initial guess 'x0' must be provided.")
+    
+    if grad is None:
+        grad = lambda x: approx_fprime(x, obj_fun, args=args)
+    
+    direction_der: float = np.dot(grad(x_0, *args), s)
+
+    #getting bounds a, b
+    if direction_der < 0:
+        x: np.ndarray[float]= x_0
+        k: int = 1
+        while True:
+            x += k * s
+            if grad(x, *args) > 0:
+                a: np.ndarray[float] = x - s
+                b: np.ndarray[float] = x
+                break
+            k += 1
+    
+    elif direction_der > 0:
+        x: np.ndarray[float] = x_0
+        k: int = 1
+        while True:
+            x -= k * s
+            if grad(x, *args) < 0:
+                a: np.ndarray[float] = x
+                b: np.ndarray[float] = x + s
+                break
+            k += 1
+    tol: float = options.get("tol", 1e-6)
+    maxiter: int = options.get("maxiter", 1000)
+    midpoint: float = (a+b) / 2
+    
+    it: int
+    for it in range(1, maxiter+1):
+        value: float = np.dot(grad(midpoint, *args), s)
+        if value < 0:
+            a = midpoint
+        elif value >= 0:
+            b = midpoint
+        
+        midpoint = (a+b) / 2
+
+        if callback is not None:
+            callback(midpoint)
+        
+        if np.linalg.norm(gradJ(midpoint)) < tol:
+            break
+    
+    success: bool = np.linalg.norm(gradJ(midpoint)) < tol
+
+    msg: str
+    if success:
+        msg = "Optimatization successful"
+    else:
+        msg = "Optimatization failed"
+    
+    return OptimizeResult(x=(a+b)/2, success=success, message=msg,
+                          nit=it, tol=tol, interval=(a, b))
+    
+
+    
+
