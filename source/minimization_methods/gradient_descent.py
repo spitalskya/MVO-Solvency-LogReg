9347e07d521d13a72a0e95e7df57235e9a587ef4
teoPazera <pazerateo@gmail.com>
2024-04-21 12:06:25 +0200

    starting changes

--- source/minimization_methods/gradient_descent.py
+++ source/minimization_methods/gradient_descent.py
@@ -5,11 +5,39 @@ from scipy.optimize import OptimizeResult
 
 def optimalStep(obj_fun: Callable[[np.ndarray], float],
                 grad: Callable[[np.ndarray], np.ndarray],
-                x_0: np.ndarray) -> OptimizeResult:
+                x_0: np.ndarray, options={}) -> OptimizeResult:
+    """_summary_
+
+    Args:
+        obj_fun (Callable[[np.ndarray], float]): _description_
+        grad (Callable[[np.ndarray], np.ndarray]): _description_
+        x_0 (np.ndarray): _description_
+
+    Returns:
+        OptimizeResult: _description_
+    """
     pass
+    
 
 
 def constantStep(obj_fun: Callable[[np.ndarray], float],
                  grad: Callable[[np.ndarray], np.ndarray],
-                 x_0: np.ndarray) -> OptimizeResult:
+                 x_0: np.ndarray, options= {}) -> OptimizeResult:
+    """_summary_
+
+    Args:
+        obj_fun (Callable[[np.ndarray], float]): _description_
+        grad (Callable[[np.ndarray], np.ndarray]): _description_
+        x_0 (np.ndarray): _description_
+
+    Returns:
+        OptimizeResult: _description_
+    """
+    tol:float = options.get("tol", 1e-6)
+    maxiter:int = options.get("maxiter", 1000)
+    gamma = ...
+    x_k = x_0
+    while True:
+        x_k1 = x_k - gamma*grad(x_k)
+        
     pass
