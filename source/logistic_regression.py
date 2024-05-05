58ae3884191db45f18d9027794368f7d1cfbecd6
antal36 <antal.tom9@gmail.com>
2024-05-05 20:22:33 +0200

    first log reg

--- source/logistic_regression.py
+++ source/logistic_regression.py
@@ -7,28 +7,30 @@ from visualizer import Visualizer
 class LogisticRegression:
     
     
-    _u: np.ndarray
-    _v: np.ndarray
+    _u: np.ndarray[np.ndarray[int]]
+    _v: np.ndarray[int]
     
     _solution: OptimizeResult | None
     
     
-    def __init__(u: np.ndarray, v: np.ndarray) -> None:
-        pass
+    def __init__(self, u: np.ndarray, v: np.ndarray) -> None:
+        self._u = u
+        self._v = v
     
+    def objective_function(self, x: np.ndarray) -> float:
+        return np.sum((1 - self._v) * np.dot(x, self._u) + np.log(1 + np.exp(-np.dot(x, self._u))))
     
-    def objective_function(x: np.ndarray) -> float:
-        pass
-    
-    
-    def gradient(x: np.ndarray) -> np.ndarray:
-        pass
+    def gradient(self, x: np.ndarray) -> np.ndarray:
+        matrix: np.ndarray[np.ndarray[int]] = self._u
+        ones_vector: np.ndarray[1] = np.ones((matrix.shape[0], 1))
+        matrix = np.hstack((ones_vector, matrix))
+        return np.sum(np.outer(matrix.T, (1 - self._v - (1 / 1 + np.exp(np.dot(x, self._u))))))
     
     
     def get_result() -> OptimizeResult:
         pass
     
-    
     def visualize(self, ax: Axes):
         # Visualizer.visualize()
         pass
+
